c1a06d2c60e9fa87163cda9d879afb5b0c71bb7f
bolderstephanie <bolderstephanie@gmail.com>
2020-04-06 15:03:46 +0200

    Eerste opzet html files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ['Roboto', 'sans-serif'],
+        body: ['Graphik', 'sans-serif'],
+        titel: ['Dancing Script', 'serif'],
+      } ,
+      colors: {
+        rood: '#FB6453' ,
+        beige: '#FFF9F6' ,
+      }
+    },
   },
   variants: {},
   plugins: [],
 }
+
