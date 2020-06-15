fb73a4e32c67477fc75fcefd9d6a950e2a5d25b0
bolderstephanie <bolderstephanie@gmail.com>
2020-06-15 14:50:54 +0200

    Bug fix

--- config/site.config.js
+++ config/site.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
 let ROOT = process.env.PWD;
 
@@ -9,25 +9,25 @@ if (!ROOT) {
 
 const config = {
   // Your website's name, used for favicon meta tags
-  site_name: 'Static Site Boilerplate',
+  site_name: "Static Site Boilerplate",
 
   // Your website's description, used for favicon meta tags
-  site_description: 'A modern boilerplate for static website development',
+  site_description: "A modern boilerplate for static website development",
 
   // Your website's URL, used for sitemap
-  site_url: '',
+  site_url: "http://www.example.com",
 
   // Google Analytics tracking ID (leave blank to disable)
-  googleAnalyticsUA: '',
+  googleAnalyticsUA: "",
 
   // The viewport meta tag added to your HTML page's <head> tag
-  viewport: 'width=device-width,initial-scale=1',
+  viewport: "width=device-width,initial-scale=1",
 
   // Source file for favicon generation. 512x512px recommended.
-  favicon: path.join(ROOT, '/src/images/favicon.png'),
+  favicon: path.join(ROOT, "/src/images/favicon.png"),
 
   // Local development URL
-  dev_host: 'localhost',
+  dev_host: "localhost",
 
   // Local development port
   port: process.env.PORT || 8000,
@@ -36,12 +36,12 @@ const config = {
   env: process.env.NODE_ENV,
   root: ROOT,
   paths: {
-    config: 'config',
-    src: 'src',
-    dist: 'dist',
+    config: "config",
+    src: "src",
+    dist: "dist",
   },
   package: JSON.parse(
-    fs.readFileSync(path.join(ROOT, '/package.json'), { encoding: 'utf-8' }),
+    fs.readFileSync(path.join(ROOT, "/package.json"), { encoding: "utf-8" })
   ),
 };
 
