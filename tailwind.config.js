e10dfa2b785d63d0b86665d5ab8e99c2e6e27077
bolderstephanie <bolderstephanie@gmail.com>
2020-10-05 16:43:16 +0200

    about & contact pagina begin

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,15 @@ module.exports = {
         display: ['Roboto', 'sans-serif'],
         body: ['Graphik', 'sans-serif'],
         titel: ['Dancing Script', 'serif'],
+        josefin: ['Josefin Sans', 'sans-serif'],
+        
       } ,
       colors: {
         rood: '#FB6453' ,
         beige: '#FFF9F6' ,
         donkerbeige: '#F3D8D0' ,
+        achtergrondbruin: '#B5A28B',
+        textgrijs: '#F1F1F1',
       }
     },
   },
